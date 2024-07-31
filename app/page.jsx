1025b132ede6dede5fbef6ed602cffb123bf7085
Emmanuel Rangel <emmanuel.rangel.jimenez.-nd@disney.com>
2024-07-31 09:59:56 -0500

    Commit for test vercel deployment

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ import packageJson from '../package.json'
 import { useForm } from "react-hook-form"
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { 
-  Form, 
+import {
+  Form,
   FormControl,
   FormDescription,
   FormField,
@@ -17,30 +17,39 @@ export default function Home() {
   const form = useForm()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section className='flex flex-col items-center m-auto'>
-        POC Blob storage v{packageJson.version}
-        <Form {...form}>
-          <form className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>File</FormLabel>
-                  <FormControl>
-                    <Input type="file" placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-      </section>
+      <div className='flex flex-row justify-around items-center m-auto w-[80vw] h-[80vh]'>
+        <section>
+          <div>
+            POC Blob storage v{packageJson.version}
+          </div>
+          <div>
+            <Form {...form}>
+              <form className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>File</FormLabel>
+                      <FormControl>
+                        <Input type="file" placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </div>
+        </section>
+        <section>
+          <p>Empty list</p>
+        </section>
+      </div>
     </main>
   );
 }
